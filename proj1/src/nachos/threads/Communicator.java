4b43c0a4eac45be22a607f38003da45097015960
Danny Chen <chenxiaoqino@gmail.com>
2015-04-14 22:07:01 +0800

    Add test cases for communicator

--- proj1/src/nachos/threads/Communicator.java
+++ proj1/src/nachos/threads/Communicator.java
@@ -1,5 +1,7 @@
 package nachos.threads;
 
+import java.util.Random;
+
 import nachos.machine.*;
 
 /**
@@ -10,6 +12,167 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
+	static int sent;
+	static int rcvd;
+	static int rcv_acc;
+	static Communicator CTcomm; 
+	private static class CaseTestHelperRecv implements Runnable{
+		public void run() {
+			int a=CTcomm.listen();
+			rcvd++;
+			rcv_acc+=a;
+		}
+	}
+	private static class CaseTestHelperSend implements Runnable{
+		private int msg;
+		CaseTestHelperSend(int a){msg=a;}
+		public void run() {
+			CTcomm.speak(msg);
+			sent++;
+		}
+	}
+	private static class CaseTester0 implements Runnable{
+    	private int tcid;
+    	CaseTester0()
+    	{
+    		tcid=TestMgr.addTest("Communicator Case Test 1: 1 on 1 (no deadlock either direction)");
+    	}
+		public void run() {
+			CTcomm=new Communicator();
+
+			KThread t1=new KThread(new CaseTestHelperSend(5));
+			t1.fork();
+			KThread.currentThread().yield();
+			KThread t2=new KThread(new CaseTestHelperRecv());
+			t2.fork();
+			t2.join();t1.join();
+			
+			
+			KThread t3=new KThread(new CaseTestHelperRecv());
+			t3.fork();
+			KThread.currentThread().yield();
+			KThread t4=new KThread(new CaseTestHelperSend(5));
+			t4.fork();
+			t4.join();t3.join();
+			
+			TestMgr.finishTest(tcid);
+		}
+	}
+	
+	private static class CaseTester1 implements Runnable{
+    	private int tcid;
+    	CaseTester1()
+    	{
+    		tcid=TestMgr.addTest("Communicator Case Test 1: 5 on 2");
+    	}
+		public void run() {
+			CTcomm=new Communicator();
+			sent=rcvd=0;
+			rcv_acc=0;
+			int snd_acc=0;
+			KThread senders[]=new KThread[5];
+			for(int i=0;i<5;i++)
+			{
+				senders[i]=new KThread(new CaseTestHelperSend(i));
+				senders[i].fork();
+				snd_acc+=i;
+			}
+			KThread t1=new KThread(new CaseTestHelperRecv());
+			t1.fork();
+			KThread t2=new KThread(new CaseTestHelperRecv());
+			t2.fork();
+			t1.join();t2.join();
+			
+			TestMgr.finishTest(tcid, sent==2 && rcvd==2);
+		}
+	}
+	
+	private static class CaseTester2 implements Runnable{
+    	private int tcid;
+    	CaseTester2()
+    	{
+    		tcid=TestMgr.addTest("Communicator Case Test 2: 2 on 5");
+    	}
+		public void run() {
+			CTcomm=new Communicator();
+			sent=rcvd=0;
+			rcv_acc=0;
+			KThread senders[]=new KThread[5];
+			for(int i=0;i<5;i++)
+			{
+				senders[i]=new KThread(new CaseTestHelperRecv());
+				senders[i].fork();
+			}
+			KThread t1=new KThread(new CaseTestHelperSend(3));
+			t1.fork();
+			KThread t2=new KThread(new CaseTestHelperSend(6));
+			t2.fork();
+			t1.join();t2.join();
+			
+			TestMgr.finishTest(tcid, sent==2 && rcvd==2 && rcv_acc==3+6);
+		}
+	}
+
+	private static class CaseTester3 implements Runnable{
+    	private int tcid;
+    	CaseTester3()
+    	{
+    		tcid=TestMgr.addTest("Communicator Case Test 3: 50 on 50");
+    	}
+		public void run() {
+			CTcomm=new Communicator();
+			sent=rcvd=0;
+			rcv_acc=0;
+			int snd_acc=0;
+			int cnts=0,cntr=0;
+			Random rnd=new Random();
+			KThread actors[]=new KThread[5];
+			for(int i=0;i<100;i++)
+			{
+				int val=rnd.nextInt(100);
+				if(rnd.nextBoolean())
+				{
+					actors[i]=new KThread(new CaseTestHelperSend(val));
+					actors[i].fork();
+					snd_acc+=val;
+					cnts++;
+				}
+				else
+				{
+					actors[i]=new KThread(new CaseTestHelperRecv());
+					actors[i].fork();
+					cntr++;
+				}
+			}
+			for(int i=100-1;i>=0;i--)actors[i].join();
+			
+			TestMgr.finishTest(tcid, rcv_acc == snd_acc && sent==rcvd && rcvd==Min(cnts,cntr));
+		}
+		private int Min(int a,int b) {
+			return a<b?a:b;
+		}
+	}
+	
+	
+	public static void selfTest(){
+		System.out.println("Communicator self test started");
+    	KThread k;
+    	k=new KThread(new CaseTester0());
+    	k.setName("Communicator CT0").fork();
+    	k.join();//cannot join due to the bug of joining finished thread.
+    	k=new KThread(new CaseTester1());
+    	k.setName("Communicator CT1").fork();
+    	k.join();
+    	k=new KThread(new CaseTester2());
+    	k.setName("Communicator CT2").fork();
+    	k.join();
+    	k=new KThread(new CaseTester3());
+    	k.setName("Communicator CT3").fork();
+    	k.join();
+		System.out.println("Communicator self test finished");
+	}
+	
+	
     /**
      * Allocate a new communicator.
      */
@@ -17,7 +180,6 @@ public class Communicator {
         lock = new Lock();
         sendCV = new Condition2(lock);
         receiveCV = new Condition2(lock);
-        
     }
 
     /**
