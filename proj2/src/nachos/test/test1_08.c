50e57661372bc064ffdba01e03889287cb40ebf3
Danny Chen <chenxiaoqino@gmail.com>
2015-05-06 23:17:02 +0800

    finish test cases

--- proj2/src/nachos/test/test1_08.c
+++ proj2/src/nachos/test/test1_08.c
@@ -11,9 +11,24 @@
 char buf[BUFSIZE],buf2[BUFSIZE];
 
 char filename[20];
-int fd,size,i,j,ret;
+int fd,size,i,j,ret,pid,st;
 int main(int argc, char** argv)
 {
-	
+	if(argc>1000)
+	{
+		fd=open("test_1_08.tmp");
+		close(fd);
+		write(fd,buf,20);
+		exit(0);
+	}
+	else
+	{
+		creat("test_1_08.tmp");
+		unlink("test_1_08.tmp");
+		
+		pid=exec("test1_08.coff",1001,argv);
+		if(join(pid,&st)!=0)exit(-1);//exception
+		exit(0);
+	}
 	return 0;
 }
