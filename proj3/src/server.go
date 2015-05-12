169141ebb9761be1e32d3f598a3d95b3b6987bd6
Danny Chen <chenxiaoqino@gmail.com>
2015-05-12 13:25:01 +0800

    Added role

--- proj3/src/server.go
+++ proj3/src/server.go
@@ -17,25 +17,39 @@ func readConf(s string) map[string]string{
 	if err != nil {
         panic(err)
     }
-    fmt.Println(dat)
+    //fmt.Println(dat)
 	var udat map[string]interface{}
 	if err := json.Unmarshal(dat, &udat); err != nil {
         panic(err)
     }
-    fmt.Println(udat)
-	
-	var ret map[string]string{}
+	ret:=make(map[string]string)
 	
 	for idx, val := range udat {
-        if t.(type) == string {
-            ret[idx]=val
-        }
+		var str=val.(string)
+		ret[idx]=str
     }
+    //fmt.Println("parsed config:")
+	//fmt.Println(ret)
 	return ret
+}
+
+const PRIMARY=1
+const BACKUP=2
+func det_role() int {
+	arg_num := len(os.Args)
+	for i := 0 ; i < arg_num ;i++{
+		switch os.Args[i] {
+			case "-p": 
+				return PRIMARY
+			case "-b": 
+				return BACKUP
+		}
+	}
+	return 0
 }
   
 var(
- role = 0 //0 is uninitialized, 1 is primary, 2 is secondary
+ role = det_role() //0 is uninitialized, 1 is primary, 2 is secondary
  stage = 0 //0: cold start, 1: warm start, 2: bootstrap, 3: synced  
  conf = readConf("conf/settings.conf")
  )
@@ -49,6 +63,19 @@ func kvmanHandler(w http.ResponseWriter, r *http.Request) {
       html.EscapeString(r.URL.Path))
 }
 func main(){
+	fmt.Println("Initialized with conf:");
+	fmt.Println(conf);
+	
+	fmt.Print("My role:");
+	switch role{
+		case PRIMARY:
+			fmt.Println("Primary")
+		case BACKUP:
+			fmt.Println("Backup")
+		default :
+			fmt.Println("Unknown; please specify role as command line parameter.")
+			panic(os.Args)
+	}
   /*s := &http.Server{
     Addr: ":8088",
     Handler: nil,
@@ -59,11 +86,5 @@ func main(){
   http.HandleFunc("/kv", kvHandler)
   http.HandleFunc("/kvman", kvmanHandler)
   log.Fatal(s.ListenAndServe())*/
-	arg_num := len(os.Args)
-	fmt.Printf("the num of input is %d\n",arg_num)
-
-	fmt.Printf("they are :\n")
-	for i := 0 ; i < arg_num ;i++{
-		fmt.Println(os.Args[i])
-	}
+	
 }
