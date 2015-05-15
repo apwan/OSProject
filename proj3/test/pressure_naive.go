d4e7da1a6eb6a44533d5cc47c829ef80a9f3196a
Danny Chen <chenxiaoqino@gmail.com>
2015-05-15 23:30:06 +0800

    finished tester; not yet tested

--- proj3/test/pressure_naive.go
+++ proj3/test/pressure_naive.go
@@ -2,9 +2,11 @@ package main
 
 import(
   "net/http"
+  "net/url"
   "fmt"
   "time"
   "strconv"
+  "math/rand"
   "sort"
   "os"
   "strings"
@@ -73,14 +75,60 @@ func naive_HTTP(url string, data_enc string, post bool) (string, error) {
 	}
 }
  
-func do_insert(key string, value string, c chan time.Duration){
+var insert_succ=0 
+func do_insert(i int, c chan time.Duration){
+	key,value:=get_key(i),get_value(i)
 	start := time.Now()
-	naive_HTTP(kvURL+"insert","key="+key+"&value="+value,true)
+	ret,err:=naive_HTTP(kvURL+"insert","key="+url.QueryEscape(key)+"&value="+url.QueryEscape(value),true)
 	c<- time.Since(start)
+	if err==nil {
+		var udat map[string]interface{}
+		if err2 := json.Unmarshal([]byte(ret), &udat); err2 == nil {
+			if udat["success"]== true {
+				insert_succ+=1
+			}
+		}	
+	}
 } 
-//func do_remove 
-
-
+var get_succ=0 
+func do_get(i int, c chan time.Duration){
+	key,value:=get_key(i),get_value(i)
+	start := time.Now()
+	ret,err:=naive_HTTP(kvURL+"get","key="+url.QueryEscape(key),true)
+	c<- time.Since(start)
+	if err==nil {
+		var udat map[string]interface{}
+		if err := json.Unmarshal([]byte(ret), &udat); err == nil {
+			if udat["success"]== true && udat["value"]==value {
+				get_succ+=1
+			}
+		}	
+	}
+} 
+var dummy=func()(string){
+  dummy:="TEST keyvalue long string................"
+  for i:=0; i<25;i++ {
+	dummy=dummy+ string(i%26+65)
+  }
+  r := rand.New(rand.NewSource(time.Now().UnixNano()))
+  dummy=fmt.Sprintf("rand%lf", r.Float64())+dummy+fmt.Sprintf("rand%lf", r.Float64())+":"
+  return dummy
+}()
+func get_key(i int)(string){
+	if i%10 ==0  && i<len(dummy){
+		copy:=""+dummy
+		buf:=[]byte(copy)
+		j:=i/10
+		buf[j]-=1
+		buf[j+1]+=2
+		return string(buf)
+	}
+	if i%2==0{ return strconv.Itoa(i)+dummy }
+	return dummy+strconv.Itoa(i)
+}
+func get_value(i int)(string){
+	return strconv.Itoa(i)+dummy+strconv.Itoa(i)
+}
 
 
 type duration_slice []time.Duration
@@ -89,34 +137,54 @@ func (a duration_slice) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a duration_slice) Less(i, j int) bool { return a[i] < a[j] }
 
 func main(){
-  N:=10000
+  N:=20
+  
+  //fmt.Println(dummy,"\nkey:",get_key(1),"\nvalue:",get_value(1))
   
   ret,err := naive_HTTP(kvmanURL+"dump","",false)
-  if err!=nil{
-    fmt.Println(err)
+  if err!=nil || ret!="{}"&& ret!="{\"_\":\"__\"}"{
+    fmt.Println(ret,err)
 	os.Exit(-1)
   }
-  fmt.Println(ret)
-  dummy:="TEST keyvalue long string................"
-  for i:=0; i<1000;i++ {
-	dummy=dummy+ string(i%26+65)
-  }
+  //fmt.Println(ret)
   
-  perf:=make(chan time.Duration, N)
-  
+  insert_perf:=make(chan time.Duration, N)
   for i:=0; i<N;i++ {
-	go do_insert(dummy+strconv.Itoa(i),strconv.Itoa(i)+dummy, perf)
+	go do_insert(i, insert_perf)
   }
-  
-  stat:=make(duration_slice, N)  // [N]time.Duration
-  //stat:=make([]int64, N)
+  insert_stat:=make(duration_slice, N) 
   for i:=0; i<N;i++ {
-	stat[i]= <-perf
+	insert_stat[i]= <-insert_perf
   }
-  sort.Sort(stat)
+  sort.Sort(insert_stat)
   
-  for i:=1;i<=9;i++ {
+  get_perf:=make(chan time.Duration, N)
+  for i:=0; i<N;i++ {
+	go do_get(i, get_perf)
+  }
+  get_stat:=make(duration_slice, N) 
+  for i:=0; i<N;i++ {
+	get_stat[i]= <-get_perf
+  }
+  sort.Sort(get_stat)
+  
+  time.Sleep(time.Millisecond)
+  println("Insertion: ",insert_succ,"/",N)
+  //println("get succ:",get_succ)
+  
+  var sum_inst=time.Duration(0)
+  var sum_get=time.Duration(0)
+  for i:=1;i<=N;i++ {
+	sum_inst+=time.Duration(int(insert_stat[i])/N)
+	sum_get+=time.Duration(int(get_stat[i])/N)
+  }
+  println("Average latency: ",sum_inst,"/",sum_get)
+  
+  print("Percentile latency: ")
+  
+  for i:=2;i<=9;i+=2 {
+	if(i==2){i++}
 	fmt.Print(strconv.Itoa(i*10)+"% Percentile:")
-	fmt.Println(stat[i*N/10])
+	fmt.Println(insert_stat[i*N/10])
   }
 }
