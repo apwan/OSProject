b2a2475e8236efd7054bf0ac0e4d6f85c70d40e0
Danny Chen <chenxiaoqino@gmail.com>
2015-06-25 19:40:08 +0800

    added sketch

--- proj4/src/kvpaxos/server.go
+++ proj4/src/kvpaxos/server.go
@@ -41,12 +41,54 @@ type KVPaxos struct {
 
 func (kv *KVPaxos) Get(args *GetArgs, reply *GetReply) error {
   // Your code here.
-  return nil
+  //Get need to recover empty slots. Empty slot means 
+  
+  /* step1: get paxos slot
+  while(1)
+  {
+	...
+	got ID
+  }
+  step2: ensure no empty slot before ID
+  for(i=ID-1;i>0;i--)
+  {
+	while(localcache[i]) empty
+	{
+		try from peer A, skip if I'm A
+		try from peer B, skip if...
+		try from peer C, ...
+	}
+	if(localcache[i].key==key)
+	{
+		latestvalue=localcache[i].value;
+		break;
+	}
+  }
+  step3: fill information
+  localcache[ID]=
+	type=get, key=key, value=val, 
+  return nil */
+  
+  
 }
 
 func (kv *KVPaxos) Put(args *PutArgs, reply *PutReply) error {
   // Your code here.
-
+	//put does not need to wait...
+	/* step1: get paxos slot
+	while(1)
+	{
+		new paxos position: ID
+		wait while not done
+		check if it's mine
+		if so, break
+	}
+	step2: save information to that slot
+	localcache[ID]=
+		type=put, key=key, value=val, 
+	step3: try broadcasting this slot. once is enough.	
+		*/
+	
   return nil
 }
 
