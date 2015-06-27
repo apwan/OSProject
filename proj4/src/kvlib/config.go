423ab5404ebbe97687c7bb5d0ee6fda485818548
Chen Xiaoqi <chenxq13@mails.tsinghua.edu.cn>
2015-06-28 00:00:34 +0800

    setup settings for new sys

--- proj4/src/kvlib/config.go
+++ proj4/src/kvlib/config.go
@@ -8,17 +8,6 @@ import(
   "math/rand"
 )
 
-const(
- PRIMARY=1
- BACKUP=2
-)
-const(
- COLD_START=0
- WARM_START=1
- BOOTSTRAP=2
- SYNC=3
- SHUTTING_DOWN=-1
-)
 
 type BoolResponse struct {
    Success bool `json:"success"`
@@ -33,69 +22,12 @@ type StrResponse struct {
     Value string `json:"value"`
 }
 
-type Duration_slice []time.Duration
-func (a Duration_slice) Len() int { return len(a) }
-func (a Duration_slice) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a Duration_slice) Less(i, j int) bool { return a[i] < a[j] }
 
-func GenLongStr()(string){
-  dummy:="TEST kv long str ......"
-  for i:=0; i<10;i++ {
-	dummy=dummy+ string(i%26+65)
-  }
-  r := rand.New(rand.NewSource(time.Now().UnixNano()))
-  dummy=fmt.Sprintf("rand%lf", r.Float64())+dummy+fmt.Sprintf("rand%lf", r.Float64())+":"
-  return dummy
-}
-
-func Det_role() int {
-	arg_num := len(os.Args)
-  if arg_num <= 1 {
-    return -1
-  }
-  if os.Args[1]=="-m"{ // for main tester
-    return 0
-  }
-  ret,err := strconv.Atoi(os.Args[1])
-  if(err!=nil){
-    return -1
-  }else{
-    return ret
-  }
-	for i := 0 ; i < arg_num ;i++{
-		switch os.Args[i] {
-			case "-p":
-				return PRIMARY
-			case "-b":
-				return BACKUP
-		}
-	}
-  fmt.Println("Unknown; please specify role as command line parameter.")
-  panic(os.Args)
-	return 0
-}
-func Find_port(role int, conf map[string]string) (int,int,int){
-	p,err := strconv.Atoi(conf["port"])
+func Find_Port(me int, conf map[string]string) (int){
+	p,err := strconv.Atoi(conf["port_n"+strconv.Itoa(me)])
 		if err != nil {
-			fmt.Println("Failed to parse port:"+conf["port"]);
+			fmt.Println("Failed to parse :"+"port_n"+strconv.Itoa(me)+":"+conf["port_n"+strconv.Itoa(me)]);
 			panic(err)
 		}
-	bp,err := strconv.Atoi(conf["back_port"])
-		if err != nil {
-			fmt.Println("Failed to parse back_port:"+conf["back_port"]);
-			panic(err)
-		}
-		if (bp == 0 ){
-      fmt.Println("Invalid back_port:");
-			panic(conf["back_port"])
-		}
-
-	if conf["primary"] != conf["backup"]{
-		return p,p,p
-	}
-
-	if role==PRIMARY{
-		return p,p,bp
-	}
-	return bp,p,bp
+	return p
 }
